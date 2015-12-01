8fbc46dd6938287ffd6f3224d51e52386cafe173
G Edward Gonzalez <gedwardgonzalez@gmail.com>
2015-11-30 22:37:33 -0800

    Mark winner when there is only one entry left

--- test/core_spec.js
+++ test/core_spec.js
@@ -62,6 +62,23 @@ describe('application logic', () => {
       });
       const nextState = next(state);
     })
+
+    it('marks the winner when just one entry is left', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'), 
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }), 
+        entries: List()
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        winner: 'Trainspotting'
+      }));
+    });
   });
 
   describe('vote', () => {
